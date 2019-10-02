66ff50692324ccfb44f85d984fd00f42094fb32c
Xenor <none@of.your>
2019-10-02 21:13:06 +0200

    Added a bearer Class

---
+++ Ma3d/Bearer/UserBearer.cs
@@ -0,0 +1,38 @@
+﻿using JWT;
+using JWT.Algorithms;
+using JWT.Serializers;
+using Ma3d.Data;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ma3d.Bearer
+{
+    public static class UserBearer
+    {
+        const string secret = "GQDstcKsx0NHjPOuXOYg5MbeJ1XT0uFiwDVvVBrk";
+
+        public static string GetToken(User user)
+        {
+            IJwtAlgorithm algorithm = new HMACSHA256Algorithm();
+            IJsonSerializer serializer = new JsonNetSerializer();
+            IBase64UrlEncoder urlEncoder = new JwtBase64UrlEncoder();
+            IJwtEncoder encoder = new JwtEncoder(algorithm, serializer, urlEncoder);
+
+            return encoder.Encode(user, secret);
+        }
+
+        public static User GetUser(string token)
+        {
+            IJsonSerializer serializer = new JsonNetSerializer();
+            IDateTimeProvider provider = new UtcDateTimeProvider();
+            IJwtValidator validator = new JwtValidator(serializer, provider);
+            IBase64UrlEncoder urlEncoder = new JwtBase64UrlEncoder();
+            IJwtDecoder decoder = new JwtDecoder(serializer, validator, urlEncoder);
+
+            return JsonConvert.DeserializeObject<User>(decoder.Decode(token, secret, verify: true));
+        }
+    }
+}
--- Ma3d/Controllers/V1/UserController.cs
+++ Ma3d/Controllers/V1/UserController.cs
@@ -1,14 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using JWT;
-using JWT.Algorithms;
-using JWT.Serializers;
+using Ma3d.Bearer;
 using Ma3d.Context;
 using Ma3d.Data;
 using Ma3d.DTO.V1;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 
@@ -50,11 +46,6 @@ namespace Ma3d.Controllers.V1
         [HttpPost("login")]
         public async Task<ActionResult<string>> Login()
         {
-            IJwtAlgorithm algorithm = new HMACSHA256Algorithm();
-            IJsonSerializer serializer = new JsonNetSerializer();
-            IBase64UrlEncoder urlEncoder = new JwtBase64UrlEncoder();
-            IJwtEncoder encoder = new JwtEncoder(algorithm, serializer, urlEncoder);
-
             UserLoginRequest ulr = JsonConvert.DeserializeObject<UserLoginRequest>(await Helper.ReadBody(Request));
 
             User u = UserContext.Users.FirstOrDefault(x => x.Name == ulr.Name);
@@ -62,8 +53,7 @@ namespace Ma3d.Controllers.V1
             {
                 return StatusCode(401);
             }
-
-            return encoder.Encode(u, secret);
+            return UserBearer.GetToken(u);
         }
 
         [HttpGet("me")]
@@ -74,17 +64,10 @@ namespace Ma3d.Controllers.V1
             string[] bearer = token.Split(' ');
             if(bearer.Length == 2)
             {
-                if (bearer[0] == "Bearer") return StatusCode(401);
+                if (bearer[0] != "Bearer") return StatusCode(401);
                 token = bearer[1];
 
-                IJsonSerializer serializer = new JsonNetSerializer();
-                IDateTimeProvider provider = new UtcDateTimeProvider();
-                IJwtValidator validator = new JwtValidator(serializer, provider);
-                IBase64UrlEncoder urlEncoder = new JwtBase64UrlEncoder();
-                IJwtDecoder decoder = new JwtDecoder(serializer, validator, urlEncoder);
-
-                User json = JsonConvert.DeserializeObject<User>(decoder.Decode(token, secret, verify: true));
-                return json;
+                return UserBearer.GetUser(token);
             }
 
             return StatusCode(401);
